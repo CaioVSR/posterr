712ae8a5d704a12afec40d8b5e8fa107d0ad624f
Caio Reis <caio.reis@grupoboticario.com.br>
2022-08-03 19:59:41 -0300

    Added basic configuration for modular pattern

---
+++ lib/app/app_module.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:posterr/app/modules/home/home_module.dart';
+import 'package:posterr/app/modules/profile/profile_module.dart';
+
+class AppModule extends Module {
+  @override
+  final List<Bind> binds = [];
+
+  @override
+  final List<ModularRoute> routes = [
+    ModuleRoute(Modular.initialRoute, module: HomeModule()),
+    ModuleRoute(Modular.initialRoute, module: ProfileModule()),
+  ];
+}
