0bb384290b01dc2fcfaaefc8308e3d1c51812eb1
Caio Reis <caio.reis@grupoboticario.com.br>
2022-08-04 18:41:09 -0300

    Added errorMessage to Hive Repository Error

--- lib/core/repositories/hive_repository/hive_repository_failures.dart
+++ lib/core/repositories/hive_repository/hive_repository_failures.dart
@@ -3,7 +3,7 @@ import 'dart:developer';
 import 'package:posterr/core/erros/failures.dart';
 
 enum HiveRepositoryFailuresTypes {
-  unknown('Unknown error');
+  unknown('Unknown error.');
 
   final String value;
 
@@ -14,14 +14,16 @@ class HiveRepositoryFailures implements Failures {
   late final HiveRepositoryFailuresTypes _error;
 
   HiveRepositoryFailures(this._error) {
-    errorLog();
+    _errorLog();
   }
 
   HiveRepositoryFailuresTypes get failure => _error;
 
   bool get isUnknown => _error == HiveRepositoryFailuresTypes.unknown;
 
-  void errorLog() => log(_error.value);
+  void _errorLog() => log(_error.value);
+
+  String get errorMessage => _error.value;
 
   factory HiveRepositoryFailures.asUnknown() => HiveRepositoryFailures(HiveRepositoryFailuresTypes.unknown);
 }
