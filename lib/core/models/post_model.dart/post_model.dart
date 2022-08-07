2f377e14e5c13cbf8c621323daa58c6a5ba209f8
Caio Reis <caio.reis@grupoboticario.com.br>
2022-08-07 08:59:18 -0300

    Added PostModel

--- lib/core/models/post_model.dart/post_model.dart
+++ lib/core/models/post_model.dart/post_model.dart
@@ -3,11 +3,34 @@ class PostModel {
   final String title;
   final String body;
   final String userId;
+  final List<String> repostsIds;
+  final List<String> quotesIds;
 
   PostModel({
     required this.id,
     required this.title,
     required this.body,
     required this.userId,
+    required this.repostsIds,
+    required this.quotesIds,
   });
+
+  factory PostModel.fromJson(Map<String, dynamic> json) => PostModel(
+    id: json['id'] as String,
+    title: json['title'] as String,
+    body: json['body'] as String,
+    userId: json['userId'] as String,
+    repostsIds: json['repostsIds'],
+    quotesIds: json['quotesIds'],
+    // quotesIds: (json['quotesIds'] as List).map((e) => e as String).toList(),
+  );
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'body': body,
+    'userId': userId,
+    'repostsIds': repostsIds,
+    'quotesIds': quotesIds,
+  };
 }
